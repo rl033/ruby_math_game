e73fe257350cdc378dd7a55b2278fef11ee8fd67
rl033 <raymond_033@hotmail.com>
2015-05-04 03:26:31 +0000

    player names supported. code refactored

--- ruby_math_game.rb
+++ ruby_math_game.rb
@@ -1,17 +1,21 @@
 require 'colorize'
 
 def initialize_game
+  @player1_name = ""
+  @player2_name = ""
   @player1_life = 3
   @player2_life = 3
   @type = 1
   @turn = 1
+  @count = 0
   @round = 1
 end
 
-def gen_questions(player)
+def gen_questions
   @num1 = rand(20) + 1
   @num2 = rand(20) + 1
   @type = rand(3) + 1
+  name = @turn == 1 ? @player1_name : @player2_name
   
   case @type
   when 1
@@ -23,7 +27,7 @@ def gen_questions(player)
   else
     op = ""
   end
-  return "Player #{player}, what does #{@num1} #{op} #{@num2} equal?"
+  return "#{name}, what does #{@num1} #{op} #{@num2} equal?"
 end
 
 def verify_answer(answer)
@@ -40,8 +44,14 @@ def verify_answer(answer)
   return answer.to_i == result ? true : false
 end
 
-def prompt_player_for_answer(player)
-  puts "Player #{player}, what's you answer?"
+def prompt_player_for_name
+  puts "Player #{@turn}, what's your name?"
+  @player1_name = gets.chomp if @turn == 1
+  @player2_name = gets.chomp if @turn == 2
+end
+
+def prompt_player_for_answer
+  puts @turn == 1 ? "#{@player1_name}, what's you answer?" : "#{@player2_name}, what's you answer?"
   gets.chomp
 end
 
@@ -49,53 +59,51 @@ def game_end?
   return @player1_life == 0 || @player2_life == 0 ? true : false
 end
 
-def update_status(player)
-  case player
-  when 1
-    @player1_life -= 1
-  when 2
-    @player2_life -= 1
+def update_status(result)
+  unless result
+    case @turn
+    when 1
+      @player1_life -= 1
+    when 2
+      @player2_life -= 1
+    end
+  end
+  @count += 1
+end
+
+def turn_manager
+  @turn = @turn == 1 ? 2 : 1
+  if @count == 2
+    @round += 1
+    @count = 0
   end
 end
 
 def display_result(result) 
-  puts result ? "You got this question right!".green : "You answer is wrong.".red
+  puts result ? "You got this question right!\n".green : "Sorry, your answer is wrong.\n".red
 end
 
 def display_status
-  puts "Player 1 has #{@player1_life} live(s) left."
-  puts "Player 2 has #{@player2_life} live(s) left."
+  puts "#{@player1_name} has #{@player1_life} live(s) left."
+  puts "#{@player2_name} has #{@player2_life} live(s) left." unless @round == 1 && @turn == 1
   if game_end?
-    puts "The game is over!"
-    puts "Player 1 won!" if @player2_life == 0
-    puts "Player 2 won!" if @player1_life == 0
+    puts "\nThe game is over!"
+    puts "#{@player1_name} won!" if @player2_life == 0
+    puts "#{@player2_name} won!" if @player1_life == 0
   end
   puts ""
 end
 
 initialize_game
+puts "Ruby Math Game"
 while(!game_end?)
-  puts "Ruby Math Game"
-
-  puts gen_questions(1)
-  answer = prompt_player_for_answer(1)
+  puts "Round #{@round}".blue if @count == 0
+  prompt_player_for_name if @round == 1
+  puts gen_questions
+  answer = prompt_player_for_answer
   result = verify_answer(answer) 
   display_result(result)
-  unless result
-    update_status(1)
-    display_status
-  end
-
-  break if game_end?
-  puts "Player 2, your turn"
-  puts gen_questions(2)
-  answer = prompt_player_for_answer(2)
-  result = verify_answer(answer) 
-  display_result(result)
-  unless result
-    update_status(2)
-    display_status
-  end
-
-  display_status
+  update_status(result)
+  display_status if @count == 2 || result == false
+  turn_manager
 end
